53b4a6408623d61d842ec96f012126b046a8f10d
Tobi <tobid7@outlook.de>
2021-11-14 23:03:28 +0100

    Update main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -1,14 +1,19 @@
 #include <renderd7.hpp>
 
+RenderD7::Sheet tx;
+RenderD7::Sprite splash;
 int main()
 {
     RenderD7::Init::Main();
-    
+    tx.Load("romfs:/gfx/tx.t3x");
+    splash.FromSheet(&tx, 3);
     while(RenderD7::MainLoop())
     {
         if (d7_hDown & KEY_B) RenderD7::ExitApp();
         RenderD7::OnScreen(Top);
+        
         RenderD7::DrawRect(0, 0, 20, 20, C2D_Color32(255, 255 , 255, 255));
+        splash.Draw();
         C3D_FrameEnd(0);
     }
 
