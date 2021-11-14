12ecf6d65c949652d2a29c74910d352e97f702fb
Tobi <tobid7@outlook.de>
2021-11-14 22:26:44 +0100

    nice test

--- source/main.cpp
+++ source/main.cpp
@@ -1,1 +1,17 @@
 #include <renderd7.hpp>
+
+int main()
+{
+    RenderD7::Init::Main();
+    
+    while(RenderD7::MainLoop())
+    {
+        if (d7_hDown & KEY_B) RenderD7::ExitApp();
+        RenderD7::OnScreen(top);
+        RenderD7::DrawRect(0, 0, 20, 20, C2D_Color32(255, 255 , 255, 255));
+        C3D_FrameEnd(0);
+    }
+
+    RenderD7::Exit::Main();
+    return 0;
+}
