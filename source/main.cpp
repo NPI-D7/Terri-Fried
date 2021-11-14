bc9ffa2fea816030760b4c6a38121e0e16d92bfd
Tobi <tobid7@outlook.de>
2021-11-14 22:35:27 +0100

    Update main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -7,7 +7,7 @@ int main()
     while(RenderD7::MainLoop())
     {
         if (d7_hDown & KEY_B) RenderD7::ExitApp();
-        RenderD7::OnScreen(top);
+        RenderD7::OnScreen(Top);
         RenderD7::DrawRect(0, 0, 20, 20, C2D_Color32(255, 255 , 255, 255));
         C3D_FrameEnd(0);
     }
