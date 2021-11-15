e0f368efe734fd62b1ef0b044b7c93d77e6dcab1
Tobi <tobid7@outlook.de>
2021-11-15 20:31:51 +0100

    Update Game.hpp

--- source/Game.hpp
+++ source/Game.hpp
@@ -7,4 +7,19 @@ class Game : public RenderD7::Scene
     void Draw(void) const override;
     void Logic(u32 hDown, u32 hHeld, u32 hUp, touchPosition touch) override;
     private:
+    //Spritesheet
+    RenderD7::Sheet tx;
+    //Sprites
+    RenderD7::Sprite egg;
+    RenderD7::Sprite coin;
+    RenderD7::Sprite lava;
+    RenderD7::Sprite logo;
+    RenderD7::Sprite platform;
+    RenderD7::Sprite scorebox;
+    RenderD7::Sprite splash_egg;
+
+    int screenheight = 240;
+    int screenwidth = 400;
+    double timer = 0;
+    double lavaY = screenheight - 32/2;
 };
