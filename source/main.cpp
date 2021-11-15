af2ca228d9117c99b8106676203c5bee6b7bdac8
Tobi <tobid7@outlook.de>
2021-11-15 14:29:51 +0100

    Update main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -48,7 +48,7 @@ int main()
         egg.Draw();
         lava.Draw();
         C3D_FrameEnd(0);
-        lavaY = screenheight - 43 - sin(timer) * 5;
+        lavaY = screenheight - 16 - sin(timer) * 5;
         timer += 0.05/2;
         lava.SetPos(0, lavaY);
     }
