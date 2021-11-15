7b01b341ff737644b7c170626e2db350a072d6af
Tobi <tobid7@outlook.de>
2021-11-15 20:33:11 +0100

    Update Game.cpp

--- source/Game.cpp
+++ source/Game.cpp
@@ -2,15 +2,38 @@
 
 Game::Game()
 {
+    //Load Spritesheet
+    tx.Load("romfs:/gfx/tx.t3x");
+    //Load Sprites
+    egg.FromSheet(&tx, 0);
+    coin.FromSheet(&tx, 1);
+    lava.FromSheet(&tx, 2);
+    logo.FromSheet(&tx, 3);
+    platform.FromSheet(&tx, 4);
+    scorebox.FromSheet(&tx, 5);
+    splash_egg.FromSheet(&tx, 6);
+    //Scale the Sprites
+    egg.SetScale(0.5, 0.5);
+    coin.SetScale(0.5, 0.5);
+    lava.SetScale(0.5, 0.5);
+    logo.SetScale(0.5, 0.5);
+    platform.SetScale(0.5, 0.5);
+    scorebox.SetScale(0.5, 0.5);
+    splash_egg.SetScale(0.5, 0.5);
 
 }
 
 void Game::Draw(void) const
 {
-
+    RenderD7::OnScreen(Top);
+    RenderD7::DrawRect(0, 0, screenwidth, screenheight, RenderD7::Color::Hex("#ECE2DE"));
+    egg.Draw();
+    lava.Draw();
 }
 
 void Game::Logic(u32 hDown, u32 hHeld, u32 hUp, touchPosition touch)
 {
-
+    lavaY = screenheight - 16 - sin(timer) * 5;
+    timer += 0.05/2;
+    lava.SetPos(0, lavaY);
 }
