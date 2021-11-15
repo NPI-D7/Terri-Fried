297245feb7e34154eddac43851a2be3f0cc80f13
Tobi <tobid7@outlook.de>
2021-11-15 15:38:11 +0100

    Create game.hpp

---
+++ source/game.hpp
@@ -0,0 +1,10 @@
+#include <renderd7.hpp>
+
+class Game : public RenderD7::Scene
+{
+    public:
+    Game();
+    void Draw(void) const override;
+    void Logic(u32 hDown, u32 hHeld, u32 hUp, touchPosition touch) override;
+    private:
+};
