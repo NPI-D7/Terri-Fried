156507a5a1fe012af708bee2237d3887e7195b8c
Tobi <tobid7@outlook.de>
2021-11-16 06:48:34 +0100

    Update main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -1,11 +1,13 @@
 #include <renderd7.hpp>
 #include <time.h>
 
+#include "Game.hpp"
+
 int main()
 {
     RenderD7::Init::Main();
     srand(time(NULL));
-    
+    RenderD7::Scene::Load(std::make_unique<Game>());
     while(RenderD7::MainLoop())
     {
         if (d7_hDown & KEY_B) RenderD7::ExitApp();
