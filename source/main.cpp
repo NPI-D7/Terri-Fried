c2f4516d0d1c5acd6baf49682214fb65fd7f6242
Tobi <tobid7@outlook.de>
2021-11-17 18:34:27 +0100

    Update main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -16,7 +16,7 @@ int main()
         RenderD7::Scene::doLogic(d7_hDown, d7_hHeld, d7_hUp, d7_touch);
         C3D_FrameEnd(0);
     }
-
+    RenderD7::Exit::NdspFirm();
     RenderD7::Exit::Main();
     return 0;
 }
