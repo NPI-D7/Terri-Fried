47cebb3a92d9f7ebddd910209cfdfd9893bed5b4
Tobi <tobid7@outlook.de>
2021-11-17 18:32:55 +0100

    Update Game.hpp

--- source/Game.hpp
+++ source/Game.hpp
@@ -4,6 +4,7 @@ class Game : public RenderD7::Scene
 {
     public:
     Game();
+    ~Game();
     void Draw(void) const override;
     void Logic(u32 hDown, u32 hHeld, u32 hUp, touchPosition touch) override;
     private:
